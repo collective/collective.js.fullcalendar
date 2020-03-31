91d08188e37c8d4009a6614ee694ebcc839fe3fb
Laurent Lasudry <lasudry@gmail.com>
2020-03-31 23:00:22 +0200

    Improve / fix README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.6.5.dev0'
 setup(name='collective.js.fullcalendar',
       version=version,
       description="Plone package for integrating fullcalendar JS",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
